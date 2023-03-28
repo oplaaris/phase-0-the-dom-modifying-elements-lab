d0cd7a7e9c88dd86e431279bd566dd9cf2d2521b
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-03-28 12:08:32 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here!
+
+
+const element = document.createElement("div");
+document.body.append(element);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+
+li.textContent = "Hi there!";
+// => <li>Hi there!</li>
+console.log(li.textContent);
+// => "Hi there!"
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+  let main = document.querySelector('main#main');
+  main.remove();
+  
+  let newHeader = document.createElement('h1');
+  newHeader.id = 'victory';
+  newHeader.innerHTML = 'YOUR-NAME is the champion';
+  document.body.appendChild(newHeader);
---
+++ index2.js
@@ -0,0 +1,4 @@
+const element = document.getElementById("main");
+  element.style.height = "300px";
+  element.style.backgroundColor = "#27647B";
+  
